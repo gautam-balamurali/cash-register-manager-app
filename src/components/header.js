23b5a79524670b7aa32585576ac79277f083339b
Gautam Balamurali <gtmbalamurali@gmail.com>
2022-12-25 17:59:01 +0530

    feat:logic implementation and style integration completed

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Cash Register Manager App</h1>
+    </header>
+  );
+}
+export default Header;
---
+++ src/config/app-config.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+const AVAILABLE_NOTES = [2000, 500, 100, 20, 10, 5, 1];
+
+const NO_OF_EACH_AVAILABLE_NOTES = [];
+AVAILABLE_NOTES.forEach((note) => NO_OF_EACH_AVAILABLE_NOTES.push(""));
+
+export { AVAILABLE_NOTES, NO_OF_EACH_AVAILABLE_NOTES };
